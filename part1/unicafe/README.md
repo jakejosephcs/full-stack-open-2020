9f187306a6592363d4e199eac265d0875c4f4532
Jake Joseph <jakejosephcs@gmail.com>
2020-05-11 17:28:13 -0400

    Finish exercise 1.6

---
+++ part1/unicafe/README.md
@@ -0,0 +1,9 @@
+# Exercises 1.6.-1.14 [LINK to exercises](https://fullstackopen.com/en/part1/a_more_complex_state_debugging_react_apps#exercises-1-6-1-14)
+
+## 1.6: unicafe step1
+Like most companies, Unicafe collects feedback from its customers. Your task is to implement a web application for collecting customer feedback. There are only three options for feedback: good, neutral, and bad.
+
+The application must display the total number of collected feedback for each category. Your final application could look like this:
+![Give feedback UI](https://fullstackopen.com/static/d4fe767d6d8eb46f1dd21334f5f9e46e/14be6/13e.png)
+
+Once you refresh the page, the collected feedback is allowed to disappear.
---
+++ part1/unicafe/src/index.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+
+const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h1>Give Feedback</h1>
+      <Button text="good" onClick={() => setGood(good + 1)} />
+      <Button text="neutral" onClick={() => setNeutral(neutral + 1)} />
+      <Button text="bad" onClick={() => setBad(bad + 1)} />
+      <h1>Statistics</h1>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />,
+  document.getElementById('root')
+)
