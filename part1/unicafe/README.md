217647e47e1bd450a1418bcaeafd3b21d1f9ca93
Jake Joseph <jakejosephcs@gmail.com>
2020-05-11 18:51:21 -0400

    Finish exercise 1.9

--- part1/unicafe/README.md
+++ part1/unicafe/README.md
@@ -4,6 +4,7 @@
 Like most companies, Unicafe collects feedback from its customers. Your task is to implement a web application for collecting customer feedback. There are only three options for feedback: good, neutral, and bad.
 
 The application must display the total number of collected feedback for each category. Your final application could look like this:
+
 ![Give feedback UI](https://fullstackopen.com/static/d4fe767d6d8eb46f1dd21334f5f9e46e/14be6/13e.png)
 
 Once you refresh the page, the collected feedback is allowed to disappear.
@@ -12,3 +13,10 @@ Once you refresh the page, the collected feedback is allowed to disappear.
 Expand your application so that it shows more statistics about the gathered feedback: the total number of collected feedback, the average score (good: 1, neutral: 0, bad: -1) and the percentage of positive feedback.
 
 ![New give feedback UI](https://fullstackopen.com/static/0a5d15ae9f055a15cb469b9c9223df41/14be6/14e.png)
+
+## 1.8: unicafe step3
+Refactor your application so that displaying the statistics is extracted into its own Statistics component. The state of the application should remain in the App root component.
+
+## 1.9: unicafe step4
+Change your application to display statistics only once feedback has been gathered.
+![New give feedback UI](https://fullstackopen.com/static/b453d7533ae85dcaf3eccf342a353c58/14be6/15e.png)
--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -3,17 +3,34 @@ import ReactDOM from 'react-dom';
 
 const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
 
-const Statistics = ({text, value, sign}) =>  <p>{text}: {value} {sign}</p>
+const Statistics = (props) => {
+  if (props.all === 0) {
+    return(
+      <p>No Feedback Given</p>
+    )
+  }
+
+  return (
+    <>
+      <p>Good: {props.good}</p>
+      <p>Neutral: {props.neutral}</p>
+      <p>Bad: {props.bad}</p>
+      <p>All: {props.all}</p>
+      <p>Average: {props.average}</p>
+      <p>Positive: {props.positive} {props.sign}</p>
+    </>
+  )
+} 
 
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const total = good + neutral + bad
-  const average = (good - bad)/total
+  const all = good + neutral + bad
+  const average = (good - bad)/all
 
-  const positiveFeedback = (good/total)*100
+  const positive = (good/all)*100
 
 
   return (
@@ -23,12 +40,7 @@ const App = () => {
       <Button text="neutral" onClick={() => setNeutral(neutral + 1)} />
       <Button text="bad" onClick={() => setBad(bad + 1)} />
       <h1>Statistics</h1>
-      <Statistics text="good" value={good}/>
-      <Statistics text="neutral" value={neutral}/>
-      <Statistics text="bad" value={bad}/>
-      <Statistics text="all" value={total}/>
-      <Statistics text="average" value={average}/>
-      <Statistics text="positive" value={positiveFeedback} sign="%"/>
+      <Statistics good={good} neutral={neutral} bad={bad} all={all} average={average} positive={positive} sign="%"/>
     </div>
   )
 }
