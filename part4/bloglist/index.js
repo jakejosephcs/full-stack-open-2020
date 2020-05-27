ad1827d544cf02bd06ece30a609f45747c621f11
Jake Joseph <jakejosephcs@gmail.com>
2020-05-27 15:43:10 -0400

    Finish exercise 4.2

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,56 +1,10 @@
+const app = require('./app')
 const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-const logger = require('./utils/logger')
 const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-const blogSchema = mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number
-})
+const server = http.createServer(app)
 
-blogSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = config.MONGODB_URI
-
-mongoose
-    .connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(logger.info('Connected'))
-
-app.use(cors())
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-    Blog
-    .find({})
-    .then(blogs => {
-        response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-    const blog = new Blog(request.body)
-
-    blog
-        .save()
-        .then(result => {
-            response.status(201).json(result)
-        })
-})
-
-const PORT = config.PORT
-app.listen(PORT, () => {
-    logger.info(`Server running on port ${PORT}`)
+server.listen(config.PORT, () => {
+    logger.info(`Server running on port ${config.PORT}`)
 })
