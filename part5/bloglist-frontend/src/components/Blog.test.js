6cd6ecec2447fc54f7a99fbd089b668dab9a2614
Jake Joseph <jakejosephcs@gmail.com>
2020-06-08 14:33:23 -0400

    Finish exercise 5.14

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,22 +1,20 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
 
+const other = {
+  currentUsersBlog: true,
+  handleRemove: () => {},
+  handleLikes: () => {}
+}
 
 test('renders author and title only by default', () => {
   const blog = {
     title: 'Test title',
     author: 'Test author',
     url: 'testurl.com',
-    like: 3,
-  }
-
-
-  const other = {
-    currentUsersBlog: true,
-    handleRemove: () => {},
-    handleLikes: () => {}
+    likes: 3,
   }
 
   const component = render(
@@ -29,5 +27,28 @@ test('renders author and title only by default', () => {
   expect(component.container).toHaveTextContent(blog.author)
   expect(component.container).toHaveTextContent(blog.title)
   expect(component.container).not.toHaveTextContent(blog.url)
+
+})
+
+test('renders blog url, number of likes when VIEW is clicked', () => {
+  const blog = {
+    title: 'Test title',
+    author: 'Test author',
+    url: 'testurl.com',
+    likes: 3,
+  }
+
+  const component = render(
+    <Blog
+      blog={blog}
+      {... other}
+    />
+  )
+
+  const viewButton = component.getByText('View')
+  fireEvent.click(viewButton)
+
+  expect(component.container).toHaveTextContent(blog.url)
+  expect(component.container).toHaveTextContent(blog.likes)
 
 })
