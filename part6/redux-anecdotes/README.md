f26433310b3ec21a3fa2f0cb724da68ff5ae624c
Jake Joseph <jakejosephcs@gmail.com>
2020-06-10 13:25:19 -0400

    Finish exercise 6.3

---
+++ part6/redux-anecdotes/README.md
@@ -0,0 +1,5 @@
+# Exercises 6.3.-6.8. [LINK to exercises](https://fullstackopen.com/en/part6/flux_architecture_and_redux#exercises-6-3-6-8)
+Let's make a new version of the anecdote voting application from part 1. Take the project from this repository https://github.com/fullstack-hy2020/redux-anecdotes to base your solution on.
+
+## 6.3: anecdotes, step1
+Implement the functionality for voting anecdotes. The amount of votes must be saved to a Redux-store.
---
+++ part6/redux-anecdotes/src/App.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+
+const App = () => {
+
+  // useSelector receives a function as a paramter. The function either searches for or selects data from the 
+  // redux-store. Here we need all of the anecdotes, so our selector function returns the whole state:
+  // state => state is the same as:
+  // (state) => {
+  //   return state
+  // }
+  const anecdotes = useSelector(state => state)
+
+  // The useDispatch-hook provides any React component access to the dispatch-function of the redux-store 
+  // defined in index.js. This allows all components to make changes to the state of the redux-store.
+  const dispatch = useDispatch()
+
+  // Previously the code dispatched actions by calling the dispatch method of the redux-store: "store.dispatch"
+  // Now it does it with the dispatch-function from the useDispatch -hook.
+  // Note: { id : id } same as just doing { id }
+  const vote = (id) => {
+    dispatch({
+      type: 'VOTE',
+      data: { id }
+    })
+  }
+
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      {anecdotes.map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      )}
+      <h2>create new</h2>
+      <form>
+        <div><input /></div>
+        <button>create</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
